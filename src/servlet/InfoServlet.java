413578e062b5472232e82420029c407a306210cf
D42511 <eisuke.negishi@orizon.co.jp>
2021-05-25 16:12:57 +0900

    2021-05-25

--- src/servlet/InfoServlet.java
+++ src/servlet/InfoServlet.java
@@ -8,11 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-<<<<<<< HEAD
 import bean.ItemBean2;
-=======
-import bean.ItemBean;
->>>>>>> refs/remotes/origin/main
 
 /**
  * Servlet implementation class InfoServlet
@@ -47,39 +43,39 @@ public class InfoServlet extends HttpServlet {
 
 		if(action.equals("info1")) {
 			info1 = request.getParameter("info1");
-			ItemBean bean = new ItemBean();
+			ItemBean2 bean = new ItemBean2();
 
 		}else if(action.equals("info2")) {
 			info2 = request.getParameter("info2");
-			ItemBean bean2 = new ItemBean();
+			ItemBean2 bean2 = new ItemBean2();
 
 		}else if(action.equals("info3")) {
 			info3 = request.getParameter("info3");
-			ItemBean bean2 = new ItemBean();
+			ItemBean2 bean2 = new ItemBean2();
 
 		}else if(action.equals("info4")) {
 			info4 = request.getParameter("info4");
-			ItemBean bean = new ItemBean();
+			ItemBean2 bean = new ItemBean2();
 
 		}else if(action.equals("info5")) {
 			info5 = request.getParameter("info5");
-			ItemBean bean = new ItemBean();
+			ItemBean2 bean = new ItemBean2();
 
 		}else if(action.equals("info6")) {
 			info6 = request.getParameter("info6");
-			ItemBean bean = new ItemBean();
+			ItemBean2 bean = new ItemBean2();
 
 		}else if(action.equals("info7")) {
 			info7 = request.getParameter("info7");
-			ItemBean bean = new ItemBean();
+			ItemBean2 bean = new ItemBean2();
 
 		}else if(action.equals("info8")) {
 			info8 = request.getParameter("info8");
-			ItemBean bean = new ItemBean();
+			ItemBean2 bean = new ItemBean2();
 
 		}else if(action.equals("info9")) {
 			info9 = request.getParameter("info9");
-			ItemBean bean = new ItemBean();
+			ItemBean2 bean = new ItemBean2();
 
 		}
 	}
