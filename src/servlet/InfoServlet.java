1dad651299933d807e626520c26092b29f1979eb
D42421 <tamaki.nakajima@orizon.co.jp>
2021-05-25 15:57:09 +0900

    BEAN2とservlet

--- src/servlet/InfoServlet.java
+++ src/servlet/InfoServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.ItemBean;
+
 /**
  * Servlet implementation class InfoServlet
  */
@@ -28,6 +30,54 @@ public class InfoServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
+		String action = request.getParameter("action");
+		String info1;
+		String info2;
+		String info3;
+		String info4;
+		String info5;
+		String info6;
+		String info7;
+		String info8;
+		String info9;
+
+		if(action.equals("info1")) {
+			info1 = request.getParameter("info1");
+			ItemBean bean = new ItemBean();
+
+		}else if(action.equals("info2")) {
+			info2 = request.getParameter("info2");
+			ItemBean bean2 = new ItemBean();
+
+		}else if(action.equals("info3")) {
+			info3 = request.getParameter("info3");
+			ItemBean bean2 = new ItemBean();
+
+		}else if(action.equals("info4")) {
+			info4 = request.getParameter("info4");
+			ItemBean bean = new ItemBean();
+
+		}else if(action.equals("info5")) {
+			info5 = request.getParameter("info5");
+			ItemBean bean = new ItemBean();
+
+		}else if(action.equals("info6")) {
+			info6 = request.getParameter("info6");
+			ItemBean bean = new ItemBean();
+
+		}else if(action.equals("info7")) {
+			info7 = request.getParameter("info7");
+			ItemBean bean = new ItemBean();
+
+		}else if(action.equals("info8")) {
+			info8 = request.getParameter("info8");
+			ItemBean bean = new ItemBean();
+
+		}else if(action.equals("info9")) {
+			info9 = request.getParameter("info9");
+			ItemBean bean = new ItemBean();
+
+		}
 	}
 
 	/**
