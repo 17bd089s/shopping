b8c51418355f3a912d31e9eb7b03a7418a4b0d76
D42421 <tamaki.nakajima@orizon.co.jp>
2021-05-25 14:08:41 +0900

    BeanとServletの作成

--- src/bean/ItemBean2.java
+++ src/bean/ItemBean2.java
@@ -9,52 +9,16 @@ public class ItemBean2 {
 	/**
 	 * フィールド
 	 */
-	private int code;     // 商品番号
-	private String name;  // 商品名
-	private int price;    // 価格
-	private int quantity; // 数量
 	private String info;  //詳細情報
 
-	/**
-	 * デフォルトコンストラクタ
-	 */
-	public ItemBean2() {
-
-	}
-
-	/**
-	 * コンストラクタ
-	 * @param code  商品番号
-	 * @param name  商品名
-	 * @param price 価格
-	 *
-	 */
-	public ItemBean2(int code, String name, int price) {
-		this.code = code;
-		this.name = name;
-		this.price = price;
-	}
-
-	/**
-	 * コンストラクタ
-	 * @param code     商品番号
-	 * @param name     商品名
-	 * @param price    価格
-	 * @param quantity 数量
-	 * @param info     詳細情報
-	 */
-	public ItemBean2(int code, String name, int price, int quantity, String info ) {
-		this(code, name, price);
-		this.quantity = quantity;
+	public ItemBean2(String info) {
+		super();
 		this.info = info;
 	}
 
-	/**
-	 * アクセサメソッド群
-	 */
-
-	public int getCode() {
-		return code;
+	public ItemBean2() {
+		super();
+		// TODO 自動生成されたコンストラクター・スタブ
 	}
 
 	public String getInfo() {
@@ -65,31 +29,5 @@ public class ItemBean2 {
 		this.info = info;
 	}
 
-	public void setCode(int code) {
-		this.code = code;
-	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getPrice() {
-		return price;
-	}
-
-	public void setPrice(int price) {
-		this.price = price;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
 }
